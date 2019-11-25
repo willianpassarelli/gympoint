b4ff72a61742d1e6b5fe901185f468123ae919b3
willianpassarelli <willian.passarelli@hotmail.com>
2019-11-25 18:42:34 -0300

    enrollments administration

--- server/src/app/controllers/EnrollmentController.js
+++ server/src/app/controllers/EnrollmentController.js
@@ -1,6 +1,35 @@
 import * as Yup from 'yup';
+import { addMonths, isBefore, parseISO } from 'date-fns';
+
+import Plan from '../models/Plan';
+import Enrollment from '../models/Enrollment';
+import Student from '../models/Student';
 
 class EnrollmentController {
+  async index(req, res) {
+    const { page = 1 } = req.query;
+
+    const enrollments = await Enrollment.findAll({
+      attributes: ['start_date', 'end_date', 'price'],
+      limit: 20,
+      offset: (page - 1) * 20,
+      include: [
+        {
+          model: Student,
+          as: 'student',
+          attributes: ['id', 'name'],
+        },
+        {
+          model: Plan,
+          as: 'plan',
+          attributes: ['id', 'title', 'price'],
+        },
+      ],
+    });
+
+    res.json(enrollments);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       start_date: Yup.date().required(),
@@ -12,8 +41,76 @@ class EnrollmentController {
       return res.status(400).json({ error: 'Validations fails' });
     }
 
+    const { student_id, plan_id, start_date } = req.body;
 
-    return res.json({ msg: 'ok' });
+    if (isBefore(parseISO(start_date), new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted' });
+    }
+
+    const plan = await Plan.findOne({
+      where: { id: plan_id },
+      attributes: ['title', 'duration', 'price'],
+    });
+
+    const { name } = await Student.findOne({ where: { id: student_id } });
+
+    const end_date = addMonths(parseISO(start_date), plan.duration);
+
+    const price = plan.duration * plan.price;
+
+    const enrollment = Object.assign(req.body, { end_date, price });
+
+    const enrollmentExists = await Enrollment.findOne({
+      where: { student_id },
+    });
+
+    if (enrollmentExists) {
+      return res
+        .status(401)
+        .json({ error: 'Student already enrolled in a plan.' });
+    }
+
+    const { id } = await Enrollment.create(enrollment);
+
+    return res.json({ id, name, start_date, end_date, price, plan });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      start_date: Yup.date(),
+      student_id: Yup.number(),
+      plan_id: Yup.number(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validations fails' });
+    }
+
+    const enrollment = await Enrollment.findByPk(req.params.id);
+
+    const { start_date, plan_id } = req.body;
+
+    if (isBefore(parseISO(start_date), new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted' });
+    }
+
+    const plan = await Plan.findOne({ where: { id: plan_id } });
+
+    const end_date = addMonths(parseISO(start_date), plan.duration);
+
+    const price = plan.duration * plan.price;
+
+    await enrollment.update({ start_date, end_date, plan_id, price });
+
+    return res.json(enrollment);
+  }
+
+  async delete(req, res) {
+    const enrollment = await Enrollment.findByPk(req.params.id);
+
+    await enrollment.destroy();
+
+    return res.json(enrollment);
   }
 }
 
--- server/src/routes.js
+++ server/src/routes.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import SessionController from './app/controllers/SessionController';
 import StudentController from './app/controllers/StudentController';
 import PlanController from './app/controllers/PlanController';
+import EnrollmentController from './app/controllers/EnrollmentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -19,5 +20,10 @@ routes.post('/plans', PlanController.store);
 routes.get('/plans', PlanController.index);
 routes.put('/plans/:id', PlanController.update);
 routes.delete('/plans/:id', PlanController.delete);
+
+routes.post('/enrollment', EnrollmentController.store);
+routes.get('/enrollment', EnrollmentController.index);
+routes.put('/enrollment/:id', EnrollmentController.update);
+routes.delete('/enrollment/:id', EnrollmentController.delete);
 
 export default routes;
